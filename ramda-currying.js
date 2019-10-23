e00363a0f2297fe9b0df987c6e9a8acaff70990c
Magluf <sergio.magluf@accenture.com>
2019-10-23 17:19:10 -0300

    Renaming.

--- ramda-currying.js
+++ ramda-currying.js
@@ -5,3 +5,7 @@ console.log(greet('Good morning', 'Joe'));
 
 const Rgreet = R.curry(greet);
 console.log(Rgreet('Good afternoon')('James'));
+console.log(Rgreet('Good morning', 'Joe')); // passing the paremeters as a regular, non-curried function still works.
+
+const eveningGreeting = Rgreet('Good evening'); // partial application.
+console.log(eveningGreeting('Fred'));
